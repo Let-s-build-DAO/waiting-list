65497847187cb2d947c76c3d92de2bc209d47734
Marvellous <marvellous@chigisoft.com>
2023-12-28 21:48:59 +0100

    docs: :tada: Project Initialization
    It has Pinia, Tailwind and snackbar installed

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
+  <NuxtSnackbar />
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style></style>
---
+++ nuxt.config.ts
@@ -0,0 +1,47 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  extends: ['github:jeremy202/Dumo-starter-kit',],
+  typescript: {
+    strict: true,
+  },
+  app: {
+    head: {
+      title: 'nuxt',
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://cdn.jsdelivr.net/gh/jeremy202/Dumo-starter-kit/assets/css/style.css',
+        },
+        { rel: "icon", type: "image/x-icon", href: "/" },,
+      ],
+      meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      ],
+    },
+  },
+  css: ['@/assets/css/style.css'],
+  modules: [
+    'nuxt-snackbar',
+    '@nuxtjs/tailwindcss',
+    '@pinia/nuxt',
+  ],
+  // snackbar: {
+  //   top: true,
+  //   right: true,
+  //   duration: 2000,
+  //   success: '#24b57a',
+  //   error: '#c33434',
+  // },
+  runtimeConfig: {
+    public: {
+      apiBaseUrl: '',
+    },
+  },
+  imports: {
+    dirs: ['store'], // auto import from store directory
+    autoImport: true
+  },
+  devtools: {
+    enabled: true,
+  },
+})
